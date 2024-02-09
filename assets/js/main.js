23c1f6b85591c7254d20884f749d80747dcb7715
Mark Blaze <markblazedotdev@protonmail.com>
2024-02-09 11:54:18 -0500

    Added auto update copyright year using js

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,3 +82,6 @@ sr.reveal('.home__card', {delay: 600, distance: '100px', interval: 100})
 sr.reveal('.about__data, .join__image', {origin: 'right'})
 sr.reveal('.about__image, .join__data', {origin: 'left'})
 sr.reveal('.popular__card', {interval: 200})
+
+/*=============== AUTO COPYRIGHT YEAR UPDATE ===============*/
+document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
